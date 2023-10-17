0ac0f8fdf5070229e6f82230adc6e0971247cfad
Tom Scott <tom.scott45456@gmail.com>
2023-10-17 18:57:33 +0100

    Player moving from side to side + gravity
    The Velocity property was used to move the player scene left and right using the arrow keys and the MoveAndSlide() function

--- player.cs
+++ player.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class player : CharacterBody2D
 {
+	private Vector2 m_velocity = Vector2.Zero;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -12,5 +14,27 @@ public partial class player : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	// Called every physics update of the game, by default it's 60u/s
+	public override void _PhysicsProcess(double delta)
+	{
+		m_velocity.Y += 2.0f;
+
+		if (Input.IsActionPressed("ui_right"))
+		{
+			m_velocity.X = 50.0f;
+		}
+		else if (Input.IsActionPressed("ui_left"))
+		{
+			m_velocity.X = -50.0f;
+		}
+		else
+		{
+			m_velocity.X = 0.0f;
+		}
+
+		Velocity = m_velocity;
+		MoveAndSlide();
 	}
 }
